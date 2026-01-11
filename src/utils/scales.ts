9a9e19740d2b4849fb1688806df6b7ed26448c84
Bilal <tringingly@gmail.com>
2026-01-11 09:40:43 -0800

    include octave in most scales

--- src/utils/scales.ts
+++ src/utils/scales.ts
@@ -7,12 +7,12 @@ export interface ScaleDefinition {
 
 export const SCALE_DEFINITIONS: Record<ScaleName, ScaleDefinition> = {
   major: { name: 'Major', intervals: [0, 2, 4, 5, 7, 9, 11, 12] },
-  minor: { name: 'Minor', intervals: [0, 2, 3, 5, 7, 8, 10] },
-  'pentatonic-major': { name: 'Pentatonic Major', intervals: [0, 2, 4, 7, 9] },
-  'pentatonic-minor': { name: 'Pentatonic Minor', intervals: [0, 3, 5, 7, 10] },
-  blues: { name: 'Blues', intervals: [0, 3, 5, 6, 7, 10] },
-  dorian: { name: 'Dorian', intervals: [0, 2, 3, 5, 7, 9, 10] },
-  mixolydian: { name: 'Mixolydian', intervals: [0, 2, 4, 5, 7, 9, 10] },
+  minor: { name: 'Minor', intervals: [0, 2, 3, 5, 7, 8, 10, 12] },
+  'pentatonic-major': { name: 'Pentatonic Major', intervals: [0, 2, 4, 7, 9, 12] },
+  'pentatonic-minor': { name: 'Pentatonic Minor', intervals: [0, 3, 5, 7, 10, 12] },
+  blues: { name: 'Blues', intervals: [0, 3, 5, 6, 7, 10, 12] },
+  dorian: { name: 'Dorian', intervals: [0, 2, 3, 5, 7, 9, 10, 12] },
+  mixolydian: { name: 'Mixolydian', intervals: [0, 2, 4, 5, 7, 9, 10, 12] },
   chromatic: { name: 'Chromatic', intervals: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11] },
 }
 
